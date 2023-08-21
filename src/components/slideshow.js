4582258b67b7e1ff142350cce13813278ca938e4
Juan <juan.ortiz96@gmail.com>
2023-08-21 20:03:40 -0300

    feat: update slider

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -6,12 +6,50 @@ import Img5 from "../img/slideshowImgs/5-astral-pet-store-thumbnail-webp-smaller
 import Img6 from "../img/slideshowImgs/6-invincible_after_a_100_years_of.webp";
 import Img7 from "../img/slideshowImgs/7-One_Coin_Clear_COVER_result.webp";
 import { useState, useEffect } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { Slide } from "react-slideshow-image";
 
 function Slideshow() {
   const [selectedSpan, setSelectedSpan] = useState(0);
+  const [slideDirection, setSlideDirection] = useState("slide-right");
   const [paused, setPaused] = useState(false);
-  const handleClick = (index) => {
-    setSelectedSpan(index);
+  const sliderImages = [
+    {
+      url: "https://nightscans.org/series/tales-of-a-shining-sword/",
+      src: Img1,
+    },
+    {
+      url: "https://nightscans.org/series/dont-do-this-your-majesty/",
+      src: Img2,
+    },
+    {
+      url: "https://nightscans.org/series/rebirth-back-to-1983-to-be-a-millionaire/",
+      src: Img3,
+    },
+    {
+      url: "https://nightscans.org/series/cong-da-shu-kaishi-de-jinhua__trashed/",
+      src: Img4,
+    },
+    {
+      url: "https://nightscans.org/series/astral-pet-store/",
+      src: Img5,
+    },
+    {
+      url: "https://nightscans.org/series/invincible-after-100-years-of-seclusion/",
+      src: Img6,
+    },
+    {
+      url: "https://nightscans.org/series/shincons-one-coin-clear/",
+      src: Img7,
+    },
+  ];
+  const handleSlideChange = (newIndex) => {
+    if (newIndex > selectedSpan) {
+      setSlideDirection("slide-left");
+    } else {
+      setSlideDirection("slide-right");
+    }
+    setSelectedSpan(newIndex);
     setPaused(true);
   };
   useEffect(() => {
@@ -33,49 +71,50 @@ function Slideshow() {
 
     return () => clearInterval(spanInterval);
   }, []);
+
+  const leftClick = () => {
+    if (selectedSpan === 0) {
+      setSelectedSpan(sliderImages.length-1)
+    } else {
+      setSelectedSpan(selectedSpan-1)
+    }
+  }
+  const rightClick = () => {
+    if (selectedSpan === sliderImages.length-1) {
+      setSelectedSpan(0)
+    } else {
+      setSelectedSpan(selectedSpan+1)
+    }
+  }
+
   return (
-    <section className="w-full relative max-h-[600px]">
-      <div className="max-h-[600px] relative cursor-pointer">
-        {selectedSpan === 0 ? (
-          <a href="https://nightscans.org/series/tales-of-a-shining-sword/">
-            <img src={Img1} className="w-full max-h-[600px] object-cover" alt="" />
+    <section className="w-screen relative max-h-[500px]">
+      <div className="max-h-[500px] relative cursor-pointer">
+        <div className="w-full h-[500px] flex items-center justify-center slider" style={{backgroundImage: `url(${sliderImages[selectedSpan].src})`,
+        backgroundSize: 'cover'}}>
+          <a
+            href={sliderImages[selectedSpan].url}
+            className="w-full max-h-[500px]"
+          >
+            {/* <img
+              src={sliderImages[selectedSpan].src}
+              className="w-full max-h-[500px] object-cover"
+              alt=""
+            /> */}
           </a>
-        ) : selectedSpan === 1 ? (
-          <a href="https://nightscans.org/series/dont-do-this-your-majesty/">
-            <img src={Img2} className="w-full max-h-[600px] object-cover" alt="" />
-          </a>
-        ) : selectedSpan === 2 ? (
-          <a href="https://nightscans.org/series/rebirth-back-to-1983-to-be-a-millionaire/">
-            <img src={Img3} className="w-full max-h-[600px] object-cover" alt="" />
-          </a>
-        ) : selectedSpan === 3 ? (
-          <a href="https://nightscans.org/series/cong-da-shu-kaishi-de-jinhua__trashed/">
-            <img src={Img4} className="w-full max-h-[600px] object-cover" alt="" />
-          </a>
-        ) : selectedSpan === 4 ? (
-          <a href="https://nightscans.org/series/astral-pet-store/">
-            <img src={Img5} className="w-full max-h-[600px] object-cover" alt="" />
-          </a>
-        ) : selectedSpan === 5 ? (
-          <a href="https://nightscans.org/series/invincible-after-100-years-of-seclusion/">
-            <img src={Img6} className="w-full max-h-[600px] object-cover" alt="" />
-          </a>
-        ) : selectedSpan === 6 ? (
-          <a href="https://nightscans.org/series/shincons-one-coin-clear/">
-            <img src={Img7} className="w-full max-h-[600px] object-cover" alt="" />
-          </a>
-        ) : null}
-      </div>
-      <div className="min-w-full h-[10px] flex justify-end px-[20px] gap-[20px] absolute bottom-5">
-        {[...Array(7)].map((_, index) => (
-          <span
-            key={index}
-            className={`w-[10px] h-[10px] bg-white ${
-              selectedSpan === index ? "bg-[#01d9b9]" : "bg-opacity-30 bg-white"
-            } rounded-[50%] cursor-pointer`}
-            onClick={() => handleClick(index)}
-          ></span>
-        ))}
+          <div
+            className="absolute top-[250px] text-white w-[88px] h-[104px] cursor-pointer left-0"
+            onClick={leftClick}
+          >
+            <IoIosArrowBack className="w-[68px] h-[84px]" />
+          </div>
+          <div
+            className="absolute top-[250px] text-white w-[88px] h-[104px] cursor-pointer right-0"
+            onClick={rightClick}
+          >
+            <IoIosArrowForward className="w-[68px] h-[84px]" />
+          </div>
+        </div>
       </div>
     </section>
   );
